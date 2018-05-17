5edca2bc0189ad9580b34544b8755dde851c7e45
SamJayathilaka <samjayathilaka@gmail.com>
2018-05-17 15:04:08 +0200

    Axes added, randomized update of charts.

--- src/app/chart/chart.component.ts
+++ src/app/chart/chart.component.ts
@@ -1,8 +1,5 @@
-import { Component, OnInit, Input, OnChanges } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import {Sensor} from '../models/sensor';
-import { ActivatedRoute } from '@angular/router';
-import { Location } from '@angular/common';
-import { SensorsService } from '../sensors.service';
 import * as d3 from 'd3';
 
 @Component({
@@ -15,93 +12,84 @@ export class ChartComponent implements OnInit {
              { 'x': 40,  'y': 10}, { 'x': 60,  'y': 40},
               { 'x': 80,  'y': 5},  { 'x': 100, 'y': 60}];
 
-  max_x = 0;
-  max_y = 0;
+  w = 620;
+  h = 400;
+  padding = 30;
+  svgContainer;
+  lineFunction;
+
+  constructor() { }
 
   @Input() sensor: Sensor;
 
-  constructor(
-    private location: Location
-  ) { }
-
-  /*ngOnChanges() {
-    const lineFunction = d3.line().x(d => d.x * 5).y(d => d.y * 5).curve(d3.curveLinear);
-
-    this.setMaxBoundaries();
-
-    const axisScale = d3.scaleLinear().domain([0, 100]).range([0, this.max_x]);
-    const xAxis = d3.axisBottom(axisScale);
-    // this.maxCount = d3.max(this.sales, (d, i) => d.count);
-    // this.y = d3.scaleBand().rangeRound([0, 75])
-    //   .domain(this.sales.map((d, i) => d.product));
-    // this.x = d3.scaleLinear()
-    //   .range([0, 300])
-    //   .domain([0, this.maxCount]);
-    // // .domain([
-    // //   new Date(Date.parse('2014-01-01')),
-    // //   new Date(Date.parse('2014-04-01'))])
-    // // .range([0, 300]);
-    // this.xAxis = d3.axisBottom(this.x).ticks(4);
-    const svgContainer = d3.select('svg').attr('width', this.max_x + 30)
-                                      .attr('height', this.max_y + 20).style('border', '1px solid black').call(xAxis).append('g')
-      .attr('transform', 'translate(20, 20)');
-    svgContainer.append('path').attr('d', lineFunction(this.lineData)).attr('stroke', 'blue')
-      .attr('stroke-width', 2).attr('fill', 'none');
-    // this.rects = this.svg.selectAll('rect')
-    //   .data(this.sales);
-    // this.newRects = this.rects.enter();
-    // this.newRects.append('rect')
-    //   .attr('x', this.x(0))
-    //   .attr('y', (d, i) => this.y(d.product))
-    //   .attr('height', this.y.bandwidth())
-    //   .attr('width', (d, i) => this.x(d.count));
-  }*/
   ngOnInit() {
-    const lineFunction = d3.line().x(d => d.x * 5).y(d => d.y * 5).curve(d3.curveLinear);
+    this.svgContainer = d3.select('#chart').append('svg').attr('width',  this.w)
+      .attr('height', this.h).style('border', '1px solid black');
+    this.randomize();
+   // this.setMaxBoundaries();
+    this.draw();
+  }
 
-    this.setMaxBoundaries();
+  draw() {
+    this.svgContainer.remove();
+    this.svgContainer = d3.select('#chart').append('svg').attr('width', this.w)
+      .attr('height', this.h).style('border', '1px solid black');
+    const xScale = d3.scaleLinear()
+      .domain([0, d3.max(this.lineData, (d) => d.x)])
+      .range([this.padding, this.w - this.padding * 2]);
+    const xAxis = d3.axisBottom(xScale);
+    const yScale = d3.scaleLinear()
+      .domain([0, d3.max(this.lineData, (d) => d.y)])
+      .range([this.h - this.padding, this.padding]);
+    const yAxis = d3.axisLeft(yScale);
 
-    const axisScale = d3.scaleLinear().domain([0, 100]).range([0, this.max_x]);
-    const xAxis = d3.axisBottom(axisScale);
-    // this.maxCount = d3.max(this.sales, (d, i) => d.count);
-    // this.y = d3.scaleBand().rangeRound([0, 75])
-    //   .domain(this.sales.map((d, i) => d.product));
-    // this.x = d3.scaleLinear()
-    //   .range([0, 300])
-    //   .domain([0, this.maxCount]);
-    // // .domain([
-    // //   new Date(Date.parse('2014-01-01')),
-    // //   new Date(Date.parse('2014-04-01'))])
-    // // .range([0, 300]);
-    // this.xAxis = d3.axisBottom(this.x).ticks(4);
-    const svgContainer = d3.select('svg').attr('width', this.max_x + 30)
-      .attr('height', this.max_y + 20).style('border', '1px solid black').call(xAxis).append('g')
-      .attr('transform', 'translate(20, 20)');
-    svgContainer.append('path').attr('d', lineFunction(this.lineData)).attr('stroke', 'blue')
+    // Add Axis
+    this.svgContainer.append('g')
+      .attr('class', 'axis')
+      .attr('transform', 'translate(0,' + (this.h - this.padding) + ')')
+      .call(xAxis);
+    this.svgContainer.append('g')
+      .attr('class', 'axis')
+      .attr('transform', 'translate(' + this.padding  + ', 0)')
+      .call(yAxis);
+
+    // Draw Line
+    this.lineFunction = d3.line().x(d => xScale(d.x)).y(d => yScale(d.y)).curve(d3.curveLinear);
+    this.svgContainer.append('path').attr('d', this.lineFunction(this.lineData)).attr('stroke', 'blue')
       .attr('stroke-width', 2).attr('fill', 'none');
   }
 
-  // getSensor(): void {
-  //   const id = +this.route.snapshot.paramMap.get('id');
-  //   this.sensorService.getSensor(id).subscribe(sensor => this.sensor = sensor);
+  // setMaxBoundaries() {
+  //   for (let i = 0; i < this.lineData.length; i++) {
+  //     const tempx = this.lineData[i].x * 5;
+  //     const tempy = this.lineData[i].y * 5;
+  //
+  //     if (tempx > this.max_x) {
+  //       this.max_x = tempx;
+  //     }
+  //
+  //     if (tempy > this.max_y) {
+  //       this.max_y = tempy;
+  //     }
+  //   }
   // }
 
-  goBack(): void {
-    this.location.back();
+  randomize() {
+    this.lineData = [];
+    const numDataPoints = 20;
+    const xRange = Math.random() * 1000;
+    const yRange = Math.random() * 1000;
+    for (let i = 0; i < numDataPoints; i++) {
+      const newNum1 = Math.round(Math.random() * xRange);
+      const newNum2 = Math.round(Math.random() * yRange);
+      this.lineData.push({'x': newNum1, 'y': newNum2});
+    }
+    this.lineData.sort((x, y) => d3.ascending(x.x, y.x));
   }
 
-  setMaxBoundaries() {
-    for (let i = 0; i < this.lineData.length; i++) {
-      const tempx = this.lineData[i].x * 5;
-      const tempy = this.lineData[i].y * 5;
-
-      if (tempx > this.max_x) {
-        this.max_x = tempx;
-      }
-
-      if (tempy > this.max_y) {
-        this.max_y = tempy;
-      }
-    }
+  randomizeAndDraw() {
+    this.randomize();
+    // this.setMaxBoundaries();
+    this.draw();
   }
 }
--- src/app/scatter-plot/scatter-plot.component.ts
+++ src/app/scatter-plot/scatter-plot.component.ts
@@ -8,33 +8,79 @@ import {Sensor} from '../models/sensor';
   styleUrls: ['./scatter-plot.component.css']
 })
 export class ScatterPlotComponent implements OnInit {
-  dataset = [
-    [5, 20], [480, 90], [250, 50], [100, 33], [330, 95],
-    [410, 12], [475, 44], [25, 67], [85, 21], [220, 88]
-  ];
-  w = 520;
-  h = 150;
+  dataset = [];
+  w = 620;
+  h = 400;
+  padding = 30;
+  svgContainer;
+
   constructor() { }
 
   @Input() sensor: Sensor;
 
   ngOnInit() {
-    const svgContainer = d3.select('svg').attr('width', this.w)
+    this.svgContainer = d3.select('#chart').append('svg').attr('width', this.w)
       .attr('height', this.h).style('border', '1px solid black');
+    this.randomize();
+    this.draw();
+  }
+
+  draw() {
+    this.svgContainer.remove();
+    this.svgContainer = d3.select('#chart').append('svg').attr('width', this.w)
+      .attr('height', this.h).style('border', '1px solid black');
+    const xScale = d3.scaleLinear()
+      .domain([0, d3.max(this.dataset, (d) => d[0])])
+      .range([this.padding, this.w - this.padding * 2]);
+    const yScale = d3.scaleLinear()
+      .domain([0, d3.max(this.dataset, (d) => d[1])])
+      .range([this.h - this.padding, this.padding]);
+    const rScale = d3.scaleLinear().domain([0, d3.max(this.dataset, (d) => d[1])]).range([2, 5]);
+    const xAxis = d3.axisBottom(xScale).ticks(5);
+    const yAxis = d3.axisLeft(yScale);
 
     // circles
-    svgContainer.selectAll('circle').data(this.dataset).enter().append('circle')
-      .attr('cx', (d) => d[0])
-      .attr('cy', (d) => d[1])
-      .attr('r', (d) =>  Math.sqrt(this.h - d[1]) / 2);
+    this.svgContainer.append('g').selectAll('circle').data(this.dataset).enter().append('circle')
+      .attr('cx', (d) => xScale(d[0]))
+      .attr('cy', (d) => yScale(d[1]))
+      .attr('r', (d) => rScale(d[1]));
 
-    svgContainer.selectAll('text').data(this.dataset).enter().append('text')
-      .text((d) => '(' + d[0] + ', ' + d[1] + ')')
-      .attr('x', (d) => d[0])
-      .attr('y', (d) => d[1])
-      .attr('font-family', 'sans-serif')
-      .attr('font-size', '11px')
-      .attr('fill', 'red');
+    // Text labels for points
+    // svgContainer.selectAll('text').data(this.dataset).enter().append('text')
+    //   .text((d) => '(' + d[0] + ', ' + d[1] + ')')
+    //   .attr('x', (d) => xScale(d[0]))
+    //   .attr('y', (d) => yScale(d[1]))
+    //   .attr('font-family', 'sans-serif')
+    //   .attr('font-size', '11px')
+    //   .attr('fill', 'red');
+
+    // Add Axis
+    this.svgContainer.append('g')
+      .attr('class', 'axis')
+      .attr('transform', 'translate(0,' + (this.h - this.padding) + ')')
+      .call(xAxis);
+    this.svgContainer.append('g')
+      .attr('class', 'axis')
+      .attr('transform', 'translate(' + this.padding  + ', 0)')
+      .call(yAxis);
+
+  }
+
+  randomizeAndDraw(){
+    this.randomize();
+    this.draw();
+  }
+
+  randomize() {
+    this.dataset = [];
+    const numDataPoints = 50;
+    const xRange = Math.random() * 1000;
+    const yRange = Math.random() * 1000;
+    for (let i = 0; i < numDataPoints; i++) {
+      const newNum1 = Math.round(Math.random() * xRange);
+      const newNum2 = Math.round(Math.random() * yRange);
+      this.dataset.push([newNum1, newNum2]);
+    }
   }
 
 }
