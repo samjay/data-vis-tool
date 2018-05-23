a488157505fad5ddfbe1cf3527ffdcae9aadd141
SamJayathilaka <samjayathilaka@gmail.com>
2018-05-23 15:07:05 +0200

    Date axis added to line chart

--- src/app/chart/chart.component.ts
+++ src/app/chart/chart.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import {Sensor} from '../models/sensor';
 import * as d3 from 'd3';
+import {parseDate} from 'ngx-bootstrap/chronos';
 
 @Component({
   selector: 'app-chart',
@@ -8,15 +9,20 @@ import * as d3 from 'd3';
   styleUrls: ['./chart.component.css']
 })
 export class ChartComponent implements OnInit {
-  lineData = [ { 'x': 1,   'y': 5},  { 'x': 20,  'y': 20},
-             { 'x': 40,  'y': 10}, { 'x': 60,  'y': 40},
-              { 'x': 80,  'y': 5},  { 'x': 100, 'y': 60}];
+  lineData = [ { 'date': '15-Jun-16',   'y': 5},  { 'date': '15-Jul-16',  'y': 20},
+             { 'date': '15-Aug-16',  'y': 10}, { 'date': '15-Sep-16',  'y': 40},
+              { 'date': '15-Oct-16',  'y': 5},  { 'date': '15-Nov-16', 'y': 60},
+    { 'date': '15-Dec-16',   'y': 40},  { 'date': '15-Jan-17',  'y': 20},
+    { 'date': '15-Feb-17',  'y': 10}, { 'date': '15-Mar-17',  'y': 40},
+    { 'date': '15-Apr-17',  'y': 5},  { 'date': '15-May-17', 'y': 60}];
 
+  preparedData = [];
   w = 620;
   h = 400;
-  padding = 30;
+  padding = 25;
   svgContainer;
   lineFunction;
+  parseDate;
 
   constructor() { }
 
@@ -25,38 +31,65 @@ export class ChartComponent implements OnInit {
   ngOnInit() {
     this.svgContainer = d3.select('#chart').append('svg').attr('width',  this.w)
       .attr('height', this.h).style('border', '1px solid black');
-    this.randomize();
+    this.prepare();
+    // this.randomize();
    // this.setMaxBoundaries();
     this.draw();
+  }
+
+  prepare() {
+    this.parseDate = d3.timeParse('%d-%b-%y');
+    this.lineData.forEach((d) => {
+      this.preparedData.push({'date': this.parseDate(d.date), 'y': d.y});
+    });
   }
 
   draw() {
     this.svgContainer.remove();
     this.svgContainer = d3.select('#chart').append('svg').attr('width', this.w)
       .attr('height', this.h).style('border', '1px solid black');
-    const xScale = d3.scaleLinear()
-      .domain([0, d3.max(this.lineData, (d) => d.x)])
+
+    // x-axis
+
+    // const xScale = d3.scaleLinear()
+    //   .domain([0, d3.max(this.lineData, (d) => d.x)])
+    //   .range([this.padding, this.w - this.padding * 2]);
+    const xScale = d3.scaleTime()
+      .domain(d3.extent(this.preparedData, (d) => d.date))
+            .range([this.padding, this.w - this.padding * 2]);
+    const xScale2 = d3.scalePoint()
+      .domain(this.preparedData.map((d) => d.date))
       .range([this.padding, this.w - this.padding * 2]);
-    const xAxis = d3.axisBottom(xScale);
+    const xAxis = d3.axisBottom(xScale2).tickFormat(d3.timeFormat('%b %d')).ticks(5).tickPadding(2);
+
+    // y-axis
     const yScale = d3.scaleLinear()
-      .domain([0, d3.max(this.lineData, (d) => d.y)])
+      .domain([0, d3.max(this.preparedData, (d) => d.y)])
       .range([this.h - this.padding, this.padding]);
     const yAxis = d3.axisLeft(yScale);
 
+    // Add grid
+    this.svgContainer.append('g')
+      .attr('class', 'grid')
+      .call(d3.axisLeft(yScale)
+        .tickSize(- this.w, 0, 0)
+        .tickFormat('')
+      );
+
     // Add Axis
     this.svgContainer.append('g')
-      .attr('class', 'axis')
+      .attr('class', 'x axis')
       .attr('transform', 'translate(0,' + (this.h - this.padding) + ')')
       .call(xAxis);
     this.svgContainer.append('g')
-      .attr('class', 'axis')
-      .attr('transform', 'translate(' + this.padding  + ', 0)')
+      .attr('class', 'y axis')
+      .attr('transform', 'translate(' + this.padding + ', 0)')
       .call(yAxis);
 
     // Draw Line
-    this.lineFunction = d3.line().x(d => xScale(d.x)).y(d => yScale(d.y)).curve(d3.curveLinear);
-    this.svgContainer.append('path').attr('d', this.lineFunction(this.lineData)).attr('stroke', 'blue')
-      .attr('stroke-width', 2).attr('fill', 'none');
+    this.lineFunction = d3.line().x(d => xScale(d.date)).y(d => yScale(d.y)).curve(d3.curveBasis);
+    this.svgContainer.append('path').attr('d', this.lineFunction(this.preparedData)).attr('stroke', 'blue')
+      .attr('stroke-width', 4).attr('fill', 'none');
   }
 
   // setMaxBoundaries() {
@@ -74,22 +107,30 @@ export class ChartComponent implements OnInit {
   //   }
   // }
 
-  randomize() {
-    this.lineData = [];
-    const numDataPoints = 20;
-    const xRange = Math.random() * 1000;
-    const yRange = Math.random() * 1000;
-    for (let i = 0; i < numDataPoints; i++) {
-      const newNum1 = Math.round(Math.random() * xRange);
-      const newNum2 = Math.round(Math.random() * yRange);
-      this.lineData.push({'x': newNum1, 'y': newNum2});
-    }
-    this.lineData.sort((x, y) => d3.ascending(x.x, y.x));
-  }
+  // randomize() {
+  //   this.lineData = [];
+  //   const numDataPoints = 20;
+  //   const xRange = Math.random() * 1000;
+  //   const yRange = Math.random() * 1000;
+  //   for (let i = 0; i < numDataPoints; i++) {
+  //     const newNum1 = Math.round(Math.random() * xRange);
+  //     const newNum2 = Math.round(Math.random() * yRange);
+  //     this.lineData.push({'x': newNum1, 'y': newNum2});
+  //   }
+  //   this.lineData.sort((x, y) => d3.ascending(x.x, y.x));
+  // }
 
-  randomizeAndDraw() {
-    this.randomize();
-    // this.setMaxBoundaries();
+  // randomizeAndDraw() {
+  //   this.randomize();
+  //   // this.setMaxBoundaries();
+  //   this.draw();
+  // }
+
+  reduceDomain() {
+    this.preparedData = this.preparedData
+      .filter((d) =>
+        d.date > this.parseDate('01-Sep-16') &&
+        d.date < this.parseDate('20-Feb-17'));
     this.draw();
   }
 }
